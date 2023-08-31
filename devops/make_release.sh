beccf91c793d144641f5c5ab4f9b5c57932313d2
david <dlondono@vertical.com.co>
2023-08-31 15:23:46 -0500

    update

--- devops/make_release.sh
+++ devops/make_release.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source .env
 PUBLIC_RELEASE_NAME=$(bash devops/get_release_name.sh)
+vite build
 
 npx sentry-cli releases new ${PUBLIC_RELEASE_NAME} --finalize
 npx sentry-cli releases files ${PUBLIC_RELEASE_NAME} upload-sourcemaps ./build/_app/immutable/ --url-prefix "~/_app/immutable/"
